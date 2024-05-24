abde942f046f735f282ee21d3dbc32e14fe5a12f
Andrey <161152527+Dakez97@users.noreply.github.com>
2024-05-23 20:41:48 -0600

    Add files via upload

---
+++ Controllers/PokemonControllers.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPokemon.Fav.Business.Services.Pokes;
+using MyPokemon.Fav.Business.Services;
+
+
+namespace Progra2_Examen1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class PokemonControllers : Controller
+{
+    private readonly PokemonServices _services;
+
+    public PokemonControllers (PokemonServices services)
+    {
+        _services = services;
+    }
+
+    [HttpGet("{PokeName}")]
+
+    public async Task<ActionResult<Pokemon>> Get(string PokeName)
+    {
+        var Poke = await _services.GetPokemonFav(PokeName);
+        return Ok(Poke);
+    }
+}
